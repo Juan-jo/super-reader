7462e6d9fd1c544c0281b4b3b9d45429ae9b0660
Juan Jose HM <t_juanhm@soriana.com>
2019-07-23 18:28:30 -0500

    Add other reader

--- SuperReader/SuperReader/SuperReader.cs
+++ SuperReader/SuperReader/SuperReader.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
+using System.Linq;
+
+
 namespace SuperReader.SuperReader
 {
     static class SuperReader
@@ -28,5 +33,34 @@ namespace SuperReader.SuperReader
             }
             return ResultList;
         }
+
+
+        //get and set model
+        public static List<T> VasReader2<T>(this SqlDataReader reader) where T : class, new()
+        {
+            if (reader == null) throw new ArgumentNullException(nameof(reader));
+
+            var ResultList = new List<T>();
+            while (reader.Read())
+            {
+                var item = Activator.CreateInstance<T>();
+
+                foreach (var propiedad in typeof(T).GetProperties())
+                {
+                    convertTo = propiedad.PropertyType;
+                    
+                    if(propiedad.Name == "String")
+                        propiedad.SetValue(item, Convert.ChangeType(reader[propiedad.Name], convertTo));
+                    else
+                    {
+                        var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                    }
+                }
+                ResultList.Add(item);
+            }
+            return ResultList;
+        }
+
     }
 }
