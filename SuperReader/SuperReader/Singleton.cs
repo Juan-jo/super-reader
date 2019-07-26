e6715a88344765433c542fef4dd707d17ef9d2df
Juanjo HM <mtz230396@gmail.com>
2019-07-26 16:47:54 -0500

    Add Singleton class

---
+++ SuperReader/SuperReader/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperReader.SuperReader
+{
+    public class Singleton<T> where T: class, new()
+    {
+        #region declare properties
+        private static volatile T NInstance = null;
+
+        // Objeto para verificar si la instacia es ejecutado por hilos y evitar que se instancié varias veces
+        private static readonly object Sync = new object();
+        #endregion declare properties
+
+        public static T Instancia
+        {
+            get
+            {
+                if(NInstance == null)
+                {
+                    // Valida si no esta bloqueda la instancia
+                    lock (Sync)
+                    {
+                        if(NInstance == null)
+                        {
+                            NInstance = new T();
+                        }
+                    }
+                }
+                return NInstance;
+            }
+        }
+    }
+}
