43c5e4494b1c29ad8a73e41affe00385c2f72c03
Juanjo HM <mtz230396@gmail.com>
2019-07-31 11:30:56 -0500

    Add test

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperReader.SuperReader;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetList()
+        {
+            List<SampleModel> list;
+            using (var context = ContextConection.Instancia)
+            {
+                try
+                {
+                    context.OpenConection();
+                    list = context.getReader("SELECT * FROM [MAQE].[Orders]").VasReader<SampleModel>();
+
+                    foreach (var item in list)
+                        Console.WriteLine("Order:{0}, Client:{1}", item.CveOrder, item.ClientName);
+                }
+                finally
+                {
+                    context.CloseConnection();
+                }
+            }
+            Assert.IsInstanceOfType(list, typeof(List<SampleModel>));
+        }
+        
+    }
+}
