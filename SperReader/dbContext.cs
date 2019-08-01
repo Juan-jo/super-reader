b4819d2fc680215ef855aae60b56c6c981a31fa1
Juanjo HM <mtz230396@gmail.com>
2019-08-01 16:04:32 -0500

    Suport Dispose dbContext

--- SperReader/dbContext.cs
+++ SperReader/dbContext.cs
@@ -5,21 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
 
 namespace SperReader
 {
     public class dbContext: IDisposable
     {
+        // Other managed resource this class uses.
+        private Component component = new Component();
+        // Track whether Dispose has been called.
+        private bool disposed = false;
+
+        private IntPtr handle;
+
         // Property singleton
         public static dbContext Instancia
         {
             get { return Singleton<dbContext>.GetInstancia; }
         }
 
-        #region Config Connection
-        protected SqlConnection myConnection = null;
+        #region Constructor
+      
 
-        public dbContext() {
+        public dbContext()
+        {
+            handle = new IntPtr();
             SqlConnectionStringBuilder myBuilder = new SqlConnectionStringBuilder()
             {
                 InitialCatalog = "master",
@@ -32,6 +45,10 @@ namespace SperReader
                 ConnectionString = myBuilder.ConnectionString
             };
         }
+        #endregion
+
+        #region Config Connection
+        protected SqlConnection myConnection = null;
         
         public void OpenConnection()
         {
@@ -64,7 +81,49 @@ namespace SperReader
         #region Support Dispose
         public void Dispose()
         {
+            Dispose(true);
             GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Check to see if Dispose has already been called.
+            if (!this.disposed)
+            {
+                // If disposing equals true, dispose all managed
+                // and unmanaged resources.
+                if (disposing)
+                {
+                    // Dispose managed resources.
+                    component.Dispose();
+                }
+
+                // Call the appropriate methods to clean up
+                // unmanaged resources here.
+                // If disposing is false,
+                // only the following code is executed.
+                CloseHandle(handle);
+                handle = IntPtr.Zero;
+
+                // Note disposing has been done.
+                disposed = true;
+
+            }
+        }
+        [System.Runtime.InteropServices.DllImport("Kernel32")]
+        private extern static Boolean CloseHandle(IntPtr handle);
+
+        // Use C# destructor syntax for finalization code.
+        // This destructor will run only if the Dispose method
+        // does not get called.
+        // It gives your base class the opportunity to finalize.
+        // Do not provide destructors in types derived from this class.
+        ~dbContext()
+        {
+            // Do not re-create Dispose clean-up code here.
+            // Calling Dispose(false) is optimal in terms of
+            // readability and maintainability.
+            Dispose(false);
         }
         #endregion uport Dispose
     }
